e70fec4f354e51100a8de75cc935359242bdf1a6
Kuba Fietkiewicz <kuba.fietkiewicz@gmail.com>
2023-11-26 20:43:40 -0600

    add protocol

--- tools/scraper.py
+++ tools/scraper.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 def getWebsiteText(url='https://www.example.com'):
+    # make sure url has protocol, and if not add it
+    if not url.startswith('http://') and not url.startswith('https://'):
+        url = 'https://' + url
+
+
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     
